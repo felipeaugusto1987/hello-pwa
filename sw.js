07857571677d1e28d8c251f9c99449ae849fbc31
Felipe Augusto <felipeaugusto1987@gmail.com>
2019-06-26 11:30:45 -0300

    Migrado para o GIT

---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
+
+/*Inicia o SW e armazena todo o conteudo em cache*/
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+/*Habilita o uso do cache quando offline*/
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response){
+            return response || fetch(e.request);
+        })
+    );
+});
